8bb89a7f98f71ec6c866984a3f85dd0927fe818d
Priyanka Kamashetty <priyankakamashetty@Priyankas-MacBook-Pro.local>
2020-11-01 10:30:46 -0800

    first commit

---
+++ build/precache-manifest.e79cf000c3dc88d82bb6303a2278c7a6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503738dc46b565d8c27ce0fbb9b0d117",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc30d96d29822239ce19",
+    "url": "/static/js/2.215ee5fe.chunk.js"
+  },
+  {
+    "revision": "15d82f2e730b4e3ade61",
+    "url": "/static/js/main.30e81f54.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
